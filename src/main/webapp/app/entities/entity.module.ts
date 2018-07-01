04562c69787cda3d91623ed2227f89a461e43eab
balaso.maske@coditas.com <balaso.maske@coditas.com>
2018-07-01 22:09:11 +0530

    1st commit gateway

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,10 +1,12 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { GatewayStudentModule as StudentserviceStudentModule } from './studentservice/student/student.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
+        StudentserviceStudentModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -18,6 +18,7 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
         proxy: [{
             context: [
                 '/uaa',
+                '/studentservice',
                 /* jhipster-needle-add-entity-to-webpack - JHipster will add entity api paths here */
                 '/api',
                 '/management',
